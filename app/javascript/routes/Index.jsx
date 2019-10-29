702ac962349a9d4d21aa86650dfddfb5ee98f7cc
rochellev <rochellevroberts@utexas.edu>
2019-10-29 13:17:49 -0700

    add list view and single recipe view for recipes

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
+import RecipeList from "../components/RecipeList";
+import Recipe from "../components/Recipe";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/recipes" exact component={RecipeList} />
+      <Route path="/recipe/:id" exact component={Recipe} />
     </Switch>
   </Router>
 );
